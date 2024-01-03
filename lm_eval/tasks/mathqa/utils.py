5e45ff3c5b13d70f40d293dbfa96d047b0dcd240
Giovanni Sullutrone <giovisull@gmail.com>
2024-01-03 17:10:03 +0100

    Added yaml for template

--- lm_eval/tasks/mathqa/utils.py
+++ lm_eval/tasks/mathqa/utils.py
@@ -7,3 +7,6 @@ def doc_to_choice(doc):
         for c in re.findall(r"[abcd] \) .*?, |e \) .*?$", doc["options"])
     ]
     return choices
+
+def text_to_prompt(text: str) -> str:
+    return f'[INST] \n{text} [/INST]'
